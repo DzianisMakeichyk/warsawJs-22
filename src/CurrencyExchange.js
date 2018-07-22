de8b94356f08ab5af626be40b886142d36d9f872
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-07-22 12:44:56 +0200

    current wal

--- src/CurrencyExchange.js
+++ src/CurrencyExchange.js
@@ -1,9 +1,23 @@
 export default class CurrencyExchange {
-  constructor(currencies) {
-    this.currencies = currencies
+  constructor(data) {
+    const { names, rates } = data.reduce((accum, entry) => {
+      const { code, buy, sell } = entry;
+
+      accum.names.push(code);
+      accum.rates[code] = { buy, sell }
+
+      return accum
+    }, { names: [], rates: {} })
+
+    this.currencies = names
+    this.rates = rates
   }
 
   getCurrencyList() {
     return this.currencies
+  }
+
+  getCurrencyRate(code) {
+    return this.rates[code]
   }
 }
--- src/CurrencyExchange.test.js
+++ src/CurrencyExchange.test.js
@@ -6,17 +6,43 @@ test('CurrencyExchange powinien byc klasa', () => {
 
 describe('getCurrencyList', () => {
   test('jest funkcja', () => {
-  const exchange = new CurrencyExchange(['USD', 'EUR', 'GBP'])
+  const exchange = new CurrencyExchange([
+    { code: 'USD', buy: 3, sell: 4 },
+    { code: 'EUR', buy: 4, sell: 5 },
+    { code: 'GBP', buy: 6, sell: 10 },
+  ])
 
     expect(typeof exchange.getCurrencyList).toEqual('function')
   });
 
   test('zwraca liste walut', () => {
-    const exchange = new CurrencyExchange(['USD', 'EUR', 'GBP'])
+    const exchange = new CurrencyExchange([
+      { code: 'USD', buy: 3, sell: 4 },
+      { code: 'EUR', buy: 4, sell: 5 },
+      { code: 'GBP', buy: 6, sell: 10 },
+    ])
 
     expect(exchange.getCurrencyList()).toEqual(['USD', 'EUR', 'GBP'])
   });
 });
 
+describe('getCurrentate', () => {
+  test('powinna byc funk', () => {
+  const exchange = new CurrencyExchange(['USD', 'EUR', 'GBP'])
 
+  expect(typeof exchange.getCurrencyRate).toEqual('function')
+  })
+
+  test('zwracy kurs wymiany dla dannej waluty', () => {
+    const exchange = new CurrencyExchange([
+      { code: 'USD', buy: 3, sell: 4 },
+      { code: 'EUR', buy: 4, sell: 5 },
+      { code: 'GBP', buy: 6, sell: 10 },
+    ])
+
+    expect(exchange.getCurrencyRate('USD')).toEqual({
+      buy: 3, sell: 4
+    })
+  })
+})
 
