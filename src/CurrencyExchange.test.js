9d0ba2e617fb0c1b73e30b3be4440cb040b7042f
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-07-22 12:53:13 +0200

    add global var

--- src/CurrencyExchange.test.js
+++ src/CurrencyExchange.test.js
@@ -1,26 +1,30 @@
 import CurrencyExchange from './CurrencyExchange'
 
+let exchange
+
+beforeEach(() => {
+  exchange = new CurrencyExchange([
+    { code: 'USD', buy: 3, sell: 4 },
+    { code: 'EUR', buy: 4, sell: 5 },
+    { code: 'GBP', buy: 6, sell: 10 },
+  ])
+})
+
+afterEach(() => {
+   exchange = undefined
+})
+
 test('CurrencyExchange powinien byc klasa', () => {
   expect(CurrencyExchange.toString()).toContain('class')
 });
 
 describe('getCurrencyList', () => {
   test('jest funkcja', () => {
-  const exchange = new CurrencyExchange([
-    { code: 'USD', buy: 3, sell: 4 },
-    { code: 'EUR', buy: 4, sell: 5 },
-    { code: 'GBP', buy: 6, sell: 10 },
-  ])
 
     expect(typeof exchange.getCurrencyList).toEqual('function')
   });
 
   test('zwraca liste walut', () => {
-    const exchange = new CurrencyExchange([
-      { code: 'USD', buy: 3, sell: 4 },
-      { code: 'EUR', buy: 4, sell: 5 },
-      { code: 'GBP', buy: 6, sell: 10 },
-    ])
 
     expect(exchange.getCurrencyList()).toEqual(['USD', 'EUR', 'GBP'])
   });
@@ -34,11 +38,6 @@ describe('getCurrentate', () => {
   })
 
   test('zwracy kurs wymiany dla dannej waluty', () => {
-    const exchange = new CurrencyExchange([
-      { code: 'USD', buy: 3, sell: 4 },
-      { code: 'EUR', buy: 4, sell: 5 },
-      { code: 'GBP', buy: 6, sell: 10 },
-    ])
 
     expect(exchange.getCurrencyRate('USD')).toEqual({
       buy: 3, sell: 4
