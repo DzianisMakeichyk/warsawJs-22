2d46ad353721f138fdd83242b6ad2c7619e0966e
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-07-22 12:22:39 +0200

    group tests

--- src/CurrencyExchange.test.js
+++ src/CurrencyExchange.test.js
@@ -4,14 +4,19 @@ test('CurrencyExchange powinien byc klasa', () => {
   expect(CurrencyExchange.toString()).toContain('class')
 });
 
-test('getCurrencyList jyst funkcja', () => {
+describe('getCurrencyList', () => {
+  test('getCurrencyList jyst funkcja', () => {
   const exchange = new CurrencyExchange(['USD', 'EUR', 'GBP'])
 
-  expect(typeof exchange.getCurrencyList).toEqual('function')
+    expect(typeof exchange.getCurrencyList).toEqual('function')
+  });
+
+  test('CurrencyExchange.getCurrencyList zwraca liste walut', () => {
+    const exchange = new CurrencyExchange(['USD', 'EUR', 'GBP'])
+
+    expect(exchange.getCurrencyList()).toEqual(['USD', 'EUR', 'GBP'])
+  });
 });
 
-test('CurrencyExchange.getCurrencyList zwraca liste walut', () => {
-  const exchange = new CurrencyExchange(['USD', 'EUR', 'GBP'])
 
-  expect(exchange.getCurrencyList()).toEqual(['USD', 'EUR', 'GBP'])
-});
+
